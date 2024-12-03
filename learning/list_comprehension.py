e5d724b28e4a5add33e778814539768301a12f36
Remiasz1 <dremiasz@cps.edu>
2024-12-03 14:41:09 -0600

    Update list_comprehension.py

--- learning/list_comprehension.py
+++ learning/list_comprehension.py
@@ -1,3 +1,31 @@
+# List Comprehention a consise way to make lists in python compact and easier to read compared to traditional loops.
+#[Expression for value in iterable if condition]
+
+# doubles = []
+# for x in range(1, 11):
+#     doubles.append(x * 2)
+# print(doubles)
+
+# doubles = [x * 2 for x in range(1, 11)]
+# triples = [y * 3 for y in range(1, 11)]
+# squares = [z * z for z in range(1, 11)]
+# print(squares)
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# fruits = [fruit.upper() for fruit in fruits]
+# fruit_char = [fruit[0] for fruit in fruits]
+# print(fruit_char)
+
+# numbers = [1, -2, 3, -4, 5, -6, 8]
+# positive_nums = [num for num in numbers if num >= 0]
+# negative_nums = [num for num in numbers if num < 0]
+# even_nums = [num for num in numbers if num % 2 == 0]
+# odd_nums = [num for num in numbers if num % 2 == 1]
+
+# grades = [85, 42, 79, 90, 56, 61, 30]
+# passing_grades = [grade for grade in grades if grade >=60]
+
+# print(passing_grades)
 
 
 ################################################List comprehension###############################################
